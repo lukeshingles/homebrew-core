903abc97700c2397820e2d7e359aedba95ce0722
Luke Shingles <luke.shingles@gmail.com>
2024-08-29 10:20:14 +0100

    python-gdbm@3.13 3.13.0 (new formula)

---
+++ Formula/p/python-gdbm@3.13.rb
@@ -0,0 +1,60 @@
+class PythonGdbmAT313 < Formula
+  desc "Python interface to gdbm"
+  homepage "https://www.python.org/"
+  url "https://www.python.org/ftp/python/3.13.0/Python-3.13.0rc3.tgz"
+  sha256 "e5e8611d1bfbfda4accf8fab868712252a9c5f6aa5f13f6a5093878a9ef38412"
+  license "Python-2.0"
+
+  livecheck do
+    formula "python@3.13"
+  end
+
+  depends_on "gdbm"
+  depends_on "python@3.13"
+
+  def python3
+    "python3.13"
+  end
+
+  def install
+    xy = Language::Python.major_minor_version python3
+    python_include = if OS.mac?
+      Formula["python@#{xy}"].opt_frameworks/"Python.framework/Versions/#{xy}/include/python#{xy}"
+    else
+      Formula["python@#{xy}"].opt_include/"python#{xy}"
+    end
+
+    cd "Modules" do
+      (Pathname.pwd/"setup.py").write <<~EOS
+        from setuptools import setup, Extension
+
+        setup(name="gdbm",
+              description="#{desc}",
+              version="#{version}",
+              ext_modules = [
+                Extension("_gdbm", ["_gdbmmodule.c"],
+                          include_dirs=["#{Formula["gdbm"].opt_include}", "#{python_include}/internal"],
+                          libraries=["gdbm"],
+                          library_dirs=["#{Formula["gdbm"].opt_lib}"])
+              ]
+        )
+      EOS
+      system python3, "-m", "pip", "install", *std_pip_args(prefix: false, build_isolation: true),
+                                              "--target=#{libexec}", "."
+      rm_r libexec.glob("*.dist-info")
+    end
+  end
+
+  test do
+    testdb = testpath/"test.db"
+    system python3, "-c", <<~EOS
+      import dbm.gnu
+
+      with dbm.gnu.open("#{testdb}", "n") as db:
+        db["testkey"] = "testvalue"
+
+      with dbm.gnu.open("#{testdb}", "r") as db:
+        assert db["testkey"] == b"testvalue"
+    EOS
+  end
+end
