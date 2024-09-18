9a8b0b44ac7e1daa063ddff9721cda5a2de80d98
Luke Shingles <luke.shingles@gmail.com>
2024-09-18 22:37:52 +0100

    pycparser: add python@3.13

--- Formula/p/pycparser.rb
+++ Formula/p/pycparser.rb
@@ -4,6 +4,7 @@ class Pycparser < Formula
   url "https://files.pythonhosted.org/packages/1d/b2/31537cf4b1ca988837256c910a668b553fceb8f069bedc4b1c826024b52c/pycparser-2.22.tar.gz"
   sha256 "491c8be9c040f5390f5bf44a5b07752bd07f56edf992381b05c701439eec10f6"
   license "BSD-3-Clause"
+  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_sequoia:  "9846cc9b7b96039a4e6b8500ec1c2c0e35536bac6263916d916a4691a51ca787"
@@ -18,6 +19,7 @@ class Pycparser < Formula
 
   depends_on "python@3.11" => [:build, :test]
   depends_on "python@3.12" => [:build, :test]
+  depends_on "python@3.13" => [:build, :test]
 
   def pythons
     deps.map(&:to_formula)
